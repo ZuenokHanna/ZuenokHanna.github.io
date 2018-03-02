8942930ab70e96c2291c4e4bfd9080fd19fd2134
ZuenokHanna <zuenok.hanna@mail.ru>
2018-03-02 19:15:25 +0000

    PSD-макет ,Gulp, Sass, Quiz

---
+++ Project_13/js/common.js
@@ -0,0 +1,331 @@
+$(document).ready(function() {
+
+	$(".toggle-mnu").click(function() {
+		$(this).toggleClass("on");
+		$(".main-mnu").slideToggle();
+		return false;
+	});
+
+	$(".top").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, "slow");
+		return false;
+	});
+
+	$(".arrow-bottom").click(function() {
+		$("html, body").animate({
+			scrollTop: $($(this).attr("href")).offset().top + "120px"}, "slow"); 
+		return false;
+	});
+
+
+	$(".slider").owlCarousel({
+		items : 1,
+		nav : true,
+		navText : "",
+		loop : true,
+		autoplay : true,
+		autoplayHoverPause : true,
+		fluidSpeed : 600,
+		autoplaySpeed : 600,
+		navSpeed : 600,
+		dotsSpeed : 600,
+		dragEndSpeed : 600
+	});
+
+	$(".main-head .top_block").animated("fadeIn");
+	$(".section_2 h1").animated("zoomIn");
+	$(".section_3 .icon_wrap").animated("bounceIn");
+	$("section .a_zoomIn").animated("zoomIn");
+	$(".section_1 img").animated("bounceInDown");
+	$(".section_7 .img_wrap_two img").animated("fadeInRight");
+	$(".section_7 .img_wrap_one img").animated("fadeInLeft");
+
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+
+		fixedContentPos: false,
+		fixedBgPos: true,
+
+		overflowY: 'auto',
+
+		closeBtnInside: true,
+		preloader: false,
+
+		midClick: true,
+		removalDelay: 500,
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+	jQuery('.mm-prev-btn').hide();
+
+	var x;
+	var count;
+	var current;
+	var percent;
+	var z = [];
+
+	init();
+	getCurrentSlide();
+	goToNext();
+	goToPrev();
+	getCount();
+	// checkStatus();
+	// buttonConfig();
+	buildStatus();
+	deliverStatus();
+	submitData();
+	goBack();
+
+	function init() {
+		
+		jQuery('.mm-survey-container .mm-survey-page').each(function() {
+
+			var item;
+			var page;
+
+			item = jQuery(this);
+			page = item.data('page');
+
+			item.addClass('mm-page-'+page);
+			//item.html(page);
+
+		});
+
+	}
+
+	function getCount() {
+
+		count = jQuery('.mm-survey-page').length;
+		return count;
+
+	}
+
+	function goToNext() {
+
+		jQuery('.mm-next-btn').on('click', function() {
+			goToSlide(x);
+			getCount();
+			current = x + 1;
+			var g = current/count;
+			buildProgress(g);
+			var y = (count + 1);
+			getButtons();
+			jQuery('.mm-survey-page').removeClass('active');
+			jQuery('.mm-page-'+current).addClass('active');
+			getCurrentSlide();
+			checkStatus();
+			if( jQuery('.mm-page-'+count).hasClass('active') ){
+				if( jQuery('.mm-page-'+count).hasClass('pass') ) {
+					jQuery('.mm-finish-btn').addClass('active');
+				}
+				else {
+					jQuery('.mm-page-'+count+' .mm-survery-content .mm-survey-item').on('click', function() {
+						jQuery('.mm-finish-btn').addClass('active');
+					});
+				}
+			}
+			else {
+				jQuery('.mm-finish-btn').removeClass('active');
+				if( jQuery('.mm-page-'+current).hasClass('pass') ) {
+					jQuery('.mm-survey-container').addClass('good');
+					jQuery('.mm-survey').addClass('okay');
+				}
+				else {
+					jQuery('.mm-survey-container').removeClass('good');
+					jQuery('.mm-survey').removeClass('okay');
+				}
+			}
+			buttonConfig();
+		});
+
+
+	}
+
+	function goToPrev() {
+
+		jQuery('.mm-prev-btn').on('click', function() {
+			goToSlide(x);
+			getCount();			
+			current = (x - 1);
+			var g = current/count;
+			buildProgress(g);
+			var y = count;
+			getButtons();
+			jQuery('.mm-survey-page').removeClass('active');
+			jQuery('.mm-page-'+current).addClass('active');
+			getCurrentSlide();
+			checkStatus();
+			jQuery('.mm-finish-btn').removeClass('active');
+			if( jQuery('.mm-page-'+current).hasClass('pass') ) {
+				jQuery('.mm-survey-container').addClass('good');
+				jQuery('.mm-survey').addClass('okay');
+			}
+			else {
+				jQuery('.mm-survey-container').removeClass('good');
+				jQuery('.mm-survey').removeClass('okay');
+			}
+			buttonConfig();
+		});
+
+	}
+
+	function buildProgress(g) {
+
+		if(g > 1){
+			g = g - 1;
+		}
+		else if (g === 0) {
+			g = 1;
+		}
+		g = g * 100;
+		jQuery('.mm-survey-progress-bar').css({ 'width' : g+'%' });
+
+	}
+
+	function goToSlide(x) {
+
+		return x;
+
+	}
+
+	function getCurrentSlide() {
+
+		jQuery('.mm-survey-page').each(function() {
+
+			var item;
+
+			item = jQuery(this);
+
+			if( jQuery(item).hasClass('active') ) {
+				x = item.data('page');
+			}
+			else {
+				
+			}
+
+			return x;
+
+		});
+
+	}
+
+	function getButtons() {
+
+		if(current === 0) {
+			current = y;
+		}
+		if(current === count) {
+			jQuery('.mm-next-btn').hide();
+		}
+		else if(current === 1) {
+			jQuery('.mm-prev-btn').hide();
+		}
+		else if(current === 4) {
+			jQuery('.mm-prev-btn').hide();
+		}
+		else {
+			jQuery('.mm-next-btn').show();
+			jQuery('.mm-prev-btn').show();
+		}
+
+	}
+
+	jQuery('.mm-survey-q li input').each(function() {
+
+		var item;
+		item = jQuery(this);
+
+		jQuery(item).on('click', function() {
+			if( jQuery('input:checked').length > 0 ) {
+		    	// console.log(item.val());
+		    	jQuery('label').parent().removeClass('active');
+		    	item.closest( 'li' ).addClass('active');
+		    }
+		    else {
+				//
+			}
+		});
+
+	});
+
+	percent = (x/count) * 100;
+	jQuery('.mm-survey-progress-bar').css({ 'width' : percent+'%' });
+
+	function checkStatus() {
+		jQuery('.mm-survery-content .mm-survey-item').on('click', function() {
+			var item;
+			item = jQuery(this);
+			item.closest('.mm-survey-page').addClass('pass');
+		});
+	}
+
+	function buildStatus() {
+		jQuery('.mm-survery-content .mm-survey-item').on('click', function() {
+			var item;
+			item = jQuery(this);
+			item.addClass('bingo');
+			item.closest('.mm-survey-page').addClass('pass');
+			jQuery('.mm-survey-container').addClass('good');
+		});
+	}
+
+	function deliverStatus() {
+		jQuery('.mm-survey-item').on('click', function() {
+			if( jQuery('.mm-survey-container').hasClass('good') ){
+				jQuery('.mm-survey').addClass('okay');
+			}
+			else {
+				jQuery('.mm-survey').removeClass('okay');	
+			}
+			buttonConfig();
+		});
+	}
+
+	function lastPage() {
+		if( jQuery('.mm-next-btn').hasClass('cool') ) {
+			alert('cool');
+		}
+	}
+
+	function buttonConfig() {
+		if( jQuery('.mm-survey').hasClass('okay') ) {
+			jQuery('.mm-next-btn button').prop('disabled', false);
+		}
+		else {
+			jQuery('.mm-next-btn button').prop('disabled', true);
+		}
+	}
+
+	function submitData() {
+		jQuery('.mm-finish-btn').on('click', function() {
+			jQuery('.mm-survey-bottom').slideUp();
+			jQuery('.mm-survey-results').slideDown();
+		});
+	}
+
+
+	function goBack() {
+		jQuery('.mm-prev-btn').on('click', function() {
+			jQuery('.mm-survey-bottom').slideDown();
+			jQuery('.mm-survey-results').slideUp();
+		});
+	}
+
+
+	$("input:radio[name=radio3]").click(function() {
+		var value = $(this).val();
+		var image_name;
+		if(value == 'graphic design'){
+			image_name = "img/q3_image.jpg";
+		}else{
+			if(value == 'mobile applications'){
+				image_name = "img/q3_image2.jpg";
+			}else{
+				image_name = "img/q3_image3.jpg";
+			}
+		}
+		$('#question').attr('src', image_name);
+	});
+
+
+});
